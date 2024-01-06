d66b24b22bb749465c8cc172c2396c544a7a8ca6
ifthekharbyte <ifthekharenan.cp@gmail.com>
2024-01-07 01:06:25 +0600

    6th problem

---
+++ domino-piling.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m,n,i,domino,result;
+
+    domino = 2*1;
+
+    cin>>m>>n;
+
+    result = (m*n)/domino;
+
+    cout<<result;
+
+
+}
