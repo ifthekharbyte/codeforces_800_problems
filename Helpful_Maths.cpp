fdab031cadb2b281d6e05a8e3fa792c2ec6e8958
ifthekharbyte <ifthekharenan.cp@gmail.com>
2024-01-12 02:28:21 +0600

    problem revised

--- Helpful_Maths.cpp
+++ Helpful_Maths.cpp
@@ -33,9 +33,32 @@ int main()
         cout<<s;
     }
 
+/*
+
+REVISED SOLUTION
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i, j;
+    string s;
+    cin >> s;
+    for (i = 0; i < s.length() ; i++) {
+        for (j = 0; j < s.length(); j++) {
+            if ( s[i] !='+' && s[i] > s[j] && s[j] !='+' ) {
+                // Swap characters
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+    cout << s << endl;
+    return 0;
+}*/
 
 
-  //seond approach - take input, remove '+' . sort string, add '+' , print string.
-  //
+
 
 
